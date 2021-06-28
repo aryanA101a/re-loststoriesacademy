c90fb30dc5496e67e268c61be9a367516c9335dc
Aryan Arora <aryansinghrathore4@gmail.com>
2021-06-28 15:59:27 +0530

    added roundness to listTiles

--- lib/widget/listtileWidget.dart
+++ lib/widget/listtileWidget.dart
@@ -4,8 +4,6 @@ import 'package:loststoriesacademy/widget/widgets.dart';
 
 import '../constants/colors.dart';
 
-
-
 class PodcastListTile extends StatelessWidget {
   PodcastListTile({
     // @required this.thumbnail,
@@ -27,54 +25,53 @@ class PodcastListTile extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: SizedBox(
         child: Container(
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: ListTile(
-              contentPadding:
-                  EdgeInsets.only(left: 12, right: 8, top: 8, bottom: 8),
-              tileColor: blue_sofa,
-              leading: SizedBox(
-                  height: 50,
-                  // width: 60,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: Image.network(
-                      'https://d8skzlz85ruek.cloudfront.net/wp-content/uploads/1-Music-Industry.jpg',
-                    ),
-                  )),
-              title: Text(
-                title,
-                style: GoogleFonts.poppins(
-                    fontSize: 15,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-              subtitle: Row(
-                children: [
-                  Icon(
-                    Icons.timer,
-                    color: Colors.white70,
+          child: ListTile(
+            shape: RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(20)),
+            contentPadding:
+                EdgeInsets.only(left: 12, right: 8, top: 8, bottom: 8),
+            tileColor: blue_sofa,
+            leading: SizedBox(
+                height: 50,
+                // width: 60,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.network(
+                    'https://d8skzlz85ruek.cloudfront.net/wp-content/uploads/1-Music-Industry.jpg',
                   ),
-                  Container(
-                      margin: EdgeInsets.only(left: 5),
-                      child: Text(
-                        subtitle,
-                        style: GoogleFonts.poppins(
-                          color: Colors.white70,
-                        ),
-                      ))
-                ],
-              ),
-              trailing: InkWell(
-                onTap: onTap,
-                child: CircleAvatar(
-                  radius: 25.0,
-                  backgroundColor: Colors.blue[100],
-                  child: Icon(
-                    Icons.play_arrow,
-                    color: base_purple,
-                    size: 30,
-                  ),
+                )),
+            title: Text(
+              title,
+              style: GoogleFonts.poppins(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            subtitle: Row(
+              children: [
+                Icon(
+                  Icons.timer,
+                  color: Colors.white70,
+                ),
+                Container(
+                    margin: EdgeInsets.only(left: 5),
+                    child: Text(
+                      subtitle,
+                      style: GoogleFonts.poppins(
+                        color: Colors.white70,
+                      ),
+                    ))
+              ],
+            ),
+            trailing: InkWell(
+              onTap: onTap,
+              child: CircleAvatar(
+                radius: 25.0,
+                backgroundColor: Colors.blue[100],
+                child: Icon(
+                  Icons.play_arrow,
+                  color: base_purple,
+                  size: 30,
                 ),
               ),
             ),
@@ -106,46 +103,44 @@ class LessonListTile extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: SizedBox(
         child: Container(
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: ListTile(
-              contentPadding:
-                  EdgeInsets.only(left: 12, right: 8, top: 8, bottom: 8),
-              tileColor: blue_sofa,
-              
-              title: Text(
-                title,
-                style: GoogleFonts.poppins(
-                    fontSize: 15,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-              subtitle: Row(
-                children: [
-                  Icon(
-                    Icons.timer,
-                    color: Colors.white70,
-                  ),
-                  Container(
-                      margin: EdgeInsets.only(left: 5),
-                      child: Text(
-                        subtitle,
-                        style: GoogleFonts.poppins(
-                          color: Colors.white70,
-                        ),
-                      ))
-                ],
-              ),
-              trailing: InkWell(
-                onTap: onTap,
-                child: CircleAvatar(
-                  radius: 25.0,
-                  backgroundColor: Colors.blue[100],
-                  child: Icon(
-                    Icons.play_arrow,
-                    color: base_purple,
-                    size: 30,
-                  ),
+          child: ListTile(
+            shape: RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(20)),
+            contentPadding:
+                EdgeInsets.only(left: 12, right: 8, top: 8, bottom: 8),
+            tileColor: blue_sofa,
+            title: Text(
+              title,
+              style: GoogleFonts.poppins(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            subtitle: Row(
+              children: [
+                Icon(
+                  Icons.timer,
+                  color: Colors.white70,
+                ),
+                Container(
+                    margin: EdgeInsets.only(left: 5),
+                    child: Text(
+                      subtitle,
+                      style: GoogleFonts.poppins(
+                        color: Colors.white70,
+                      ),
+                    ))
+              ],
+            ),
+            trailing: InkWell(
+              onTap: onTap,
+              child: CircleAvatar(
+                radius: 25.0,
+                backgroundColor: Colors.blue[100],
+                child: Icon(
+                  Icons.play_arrow,
+                  color: base_purple,
+                  size: 30,
                 ),
               ),
             ),
